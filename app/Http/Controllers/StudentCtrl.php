c408caa5c4844413b32891fb6cdf014da5462eef
pabloIO <pabloIO>
2017-12-22 17:37:20 -0400

    updates on parent sign

--- app/Http/Controllers/StudentCtrl.php
+++ app/Http/Controllers/StudentCtrl.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Usuario;
+use App\Models\Estudiante;
 use DB;
 
 use App\Traits\UserTrait as UserTrait;
@@ -12,20 +13,54 @@ class StudentCtrl extends Controller
 {
 	use UserTrait;
     //
-    public function index(){
+    public function index($page){
+				$QT_PAGE = 15;
+				$PAGE = $page;
+				$TOTAL = DB::table('estudiante')
+										->join('usuario', 'estudiante.usuario_id', '=', 'usuario.id')
+										->where('usuario.activo', '=', '1')
+										->count();
+				$TOTAL_PAGES = ceil($TOTAL / $QT_PAGE);
+				$dbStudents = DB::table('estudiante')
+											->join('usuario', 'estudiante.usuario_id', '=', 'usuario.id')
+											->where('usuario.activo', '=', '1')
+											->skip($PAGE - 1)
+											->take($QT_PAGE)
+											->get();
         $students = [];
         $res = (object) null;
         try{
-            foreach (Usuario::where('activo' , '=', 1)->get() as $user) {
-                if($user->role($user->id)->rol_id == 1 AND $user->activo == 1){
-                    $students[] = UserTrait::userData($user->id);
-                }
+            foreach ($dbStudents as $student) {
+              $students[] = UserTrait::userData($student->usuario_id);
+            }
+            $res->success = true;
+            $res->students = $students;
+						$res->pages = $TOTAL_PAGES;
+            return response()->json($res);
+        }catch(\Exception $e){
+            $res->success = false;
+						$res->err = $e->getMessage();
+            $res->msg = 'Hubo un error al cargar los datos de los estudiantes';
+            return response()->json($res);
+        }
+    }
+		public function indexAdmin(){
+				$dbStudents = DB::table('estudiante')
+											->join('usuario', 'estudiante.usuario_id', '=', 'usuario.id')
+											->where('usuario.activo', '=', '1')
+											->get();
+        $students = [];
+        $res = (object) null;
+        try{
+            foreach ($dbStudents as $student) {
+              $students[] = UserTrait::userData($student->usuario_id);
             }
             $res->success = true;
             $res->students = $students;
             return response()->json($res);
         }catch(\Exception $e){
             $res->success = false;
+						$res->err = $e->getMessage();
             $res->msg = 'Hubo un error al cargar los datos de los estudiantes';
             return response()->json($res);
         }
