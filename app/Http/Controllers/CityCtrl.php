37bdd3abbfb17a025e58b403edf5cfb7ae7cdf6e
pabloIO <pabloIO>
2018-04-07 16:09:24 -0400

    update

--- app/Http/Controllers/CityCtrl.php
+++ app/Http/Controllers/CityCtrl.php
@@ -9,15 +9,24 @@ class CityCtrl extends Controller
 {
     public function index(){
     	$cities = [];
-    	foreach (Ciudad::all() as $city) {
-    		$cities[] = [
-    			'id'	=> $city->id,
-    			'label' => $city->nombre
-    		];
-    	}
-    	return response()->json([
-    		'success' => true, 
-    		'cities'	=> $cities
-    	]);
+      try{
+        foreach (Ciudad::all() as $city) {
+      		$cities[] = [
+      			'id'	=> $city->id,
+      			'label' => $city->nombre
+      		];
+      	}
+      	return response()->json([
+      		'success' => true,
+      		'cities'	=> $cities,
+          'msg'  => '200 OK',
+      	]);
+      }catch(\Exception $e){
+        return response()->json([
+      		'success' => false,
+          'err'  => $e->getMessage(),
+          'msg'  => 'Hubo un error en el servidor'
+      	]);
+      }
     }
 }
